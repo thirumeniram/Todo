9c3a480ad99fd37adc1ccb6b8d941f52746e0bbf
thirumeniram Arumugam <thirumeniram25@gmail.com>
2023-12-23 15:06:39 +0530

    Fourth commit

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,23 +1,4 @@
-// import React from "react";
 
-// const TodoForm = ({ handleSubmit, todo, setTodo, editId }) => {
-//     return (
-//       <form onSubmit={handleSubmit}>
-//         {/* Add a name attribute to your input field */}
-//         <input
-//           type="text"
-//           value={todo}
-//           onChange={(e) => setTodo(e.target.value)}
-//           name="todoInput" // This is the name attribute for the input field
-//           id="todoInput" // It's also a good practice to add an id that matches the name
-//           autoComplete="off" // This prevents the browser from trying to autocomplete the field
-//         />
-//         <button type="submit">{editId ? 'Update Todo' : 'Add Todo'}</button>
-//       </form>
-//     );
-//   };
-  
-//   export default TodoForm;
 
 import React from "react";
 
@@ -28,9 +9,9 @@ const TodoForm = ({ handleSubmit, todo, editId, setTodo }) => {
         type="text"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
-        name="todoInput" // This is the name attribute for the input field
-        id="todoInput" // It's also a good practice to add an id that matches the name
-        autoComplete="off" // This prevents the browser from trying to autocomplete the field
+        name="todoInput" 
+        id="todoInput" 
+        autoComplete="off" 
       />
       <button type="submit"> {editId ? "Edit" : "Add"}</button>
     </form>

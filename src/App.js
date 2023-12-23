1700aed02e78645b44d2325c2728bf5e539922b9
thirumeniram Arumugam <thirumeniram25@gmail.com>
2023-12-23 08:20:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  // Load todos from localStorage on initial render
+  useEffect(() => {
+    console.log('got called')
+    const storedTodos = JSON.parse(localStorage.getItem("Todos"));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  // Save todos to localStorage whenever they change
+  useEffect(() => {
+   
+    localStorage.setItem("Todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? { id: t.id, todo }
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Todo List App</h1>
+        <TodoForm
+          handleSubmit={handleSubmit}
+          todo={todo}
+          setTodo={setTodo}
+          editId={editId}
+        />
+        <TodoList
+          todos={todos}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,41 @@
+// import React from "react";
+
+// const TodoForm = ({ handleSubmit, todo, setTodo, editId }) => {
+//     return (
+//       <form onSubmit={handleSubmit}>
+//         {/* Add a name attribute to your input field */}
+//         <input
+//           type="text"
+//           value={todo}
+//           onChange={(e) => setTodo(e.target.value)}
+//           name="todoInput" // This is the name attribute for the input field
+//           id="todoInput" // It's also a good practice to add an id that matches the name
+//           autoComplete="off" // This prevents the browser from trying to autocomplete the field
+//         />
+//         <button type="submit">{editId ? 'Update Todo' : 'Add Todo'}</button>
+//       </form>
+//     );
+//   };
+  
+//   export default TodoForm;
+
+import React from "react";
+
+const TodoForm = ({ handleSubmit, todo, editId, setTodo }) => {
+  return (
+    <form className="todoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        name="todoInput" // This is the name attribute for the input field
+        id="todoInput" // It's also a good practice to add an id that matches the name
+        autoComplete="off" // This prevents the browser from trying to autocomplete the field
+      />
+      <button type="submit"> {editId ? "Edit" : "Go"}</button>
+    </form>
+  );
+};
+
+export default TodoForm;
+  
